05fe1b7b8568d67d7106616cd22ecb5872a38742
thekakester <mitcheld@mtu.edu>
2018-06-29 13:40:26 -0400

    Refactored task page code (and rev history)

--- task.php
+++ task.php
@@ -7,6 +7,7 @@ include_once('autocomplete.php');
 
 include('top.php');
 echo panel("Task Details",taskDetails());
+echo panel("Revision History",taskRevisionHistory());
 include('bottom.php');
 
 function taskDetails() {
@@ -15,7 +16,7 @@ function taskDetails() {
 	$html = "";
 	if ($tid) {
 		//$resultset=$conn->query("SELECT * FROM ((SELECT tid,MAX(id) AS id FROM tasks GROUP BY tid) AS t1 JOIN tasks AS t2 ON t1.id=t2.id) WHERE t2.tid=$tid");
-		$resultset=$conn->query("SELECT * FROM ((SELECT * FROM tasks WHERE tid=$tid) AS t1 JOIN users AS t2 ON t1.owner=t2.id) ORDER BY t1.id DESC");
+		$resultset=$conn->query("SELECT * FROM ((SELECT MAX(id) as id FROM tasks WHERE tid=$tid) t1 JOIN tasks t2 ON t1.id=t2.id JOIN users t3 ON t3.id=t2.owner)");
 		if ($row=$resultset->fetch_assoc()) {
 			$html .= "<table class='table table-bordered table-hover table-striped'>
 						<tr><th>Task</th>
@@ -49,33 +50,39 @@ function taskDetails() {
 						<td><a href='#'><span class='glyphicon glyphicon-ok'></span></a></td>
 					</tr>";
 			$html .= "</table>";
-			
-			
-			$html .= "<h2>Revision History</h2>";
-			$html .= "<table class='table table-bordered table-hover table-striped'><tr>
-							<th>Task</th>
-							<th>Description</th>
-							<th>Job</th>
-							<th>Due Date</th>
-							<th>Days Open</th>
-							<th>Owner</th>
-						</tr>";
-			while ($row=$resultset->fetch_assoc()) {
-					$dueDate=date("n/j/y",$row['dueDate']);
-					$daysOpen=ceil((time()-$row['openDate'])/(24*60*60));
-					$html .= "<tr>
-							<td><a href='task.php?tid=$row[tid]'>$row[tid]</a></td>
-							<td><a href='task.php?tid=$row[tid]'>$row[description]</a></td>
-							<td>$row[jid]</td>
-							<td>$dueDate</td>
-							<td>$daysOpen</td>
-							<td>$row[user]</td>
-						</tr>";
-			}
-			$html .= "</table>";		
-			
-			
 		}
+	}
+	return $html;
+}
+function taskRevisionHistory() {
+	global $conn;
+	$tid=get('tid');
+	$html = "";
+	if ($tid) {
+		$resultset=$conn->query("SELECT * FROM ((SELECT * FROM tasks WHERE tid=$tid) AS t1 JOIN users AS t2 ON t1.owner=t2.id) ORDER BY t1.id DESC");
+		$html .= "<table class='table table-bordered table-hover table-striped'><tr>
+						<th>Task</th>
+						<th>Description</th>
+						<th>Job</th>
+						<th>Due Date</th>
+						<th>Days Open</th>
+						<th>Owner</th>
+					</tr>";
+		while ($row=$resultset->fetch_assoc()) {
+				$dueDate=date("n/j/y",$row['dueDate']);
+				$daysOpen=ceil((time()-$row['openDate'])/(24*60*60));
+				$html .= "<tr>
+						<td><a href='task.php?tid=$row[tid]'>$row[tid]</a></td>
+						<td><a href='task.php?tid=$row[tid]'>$row[description]</a></td>
+						<td>$row[jid]</td>
+						<td>$dueDate</td>
+						<td>$daysOpen</td>
+						<td>$row[user]</td>
+					</tr>";
+		}
+		$html .= "</table>";		
+		
+		
 	}
 	return $html;
 }
